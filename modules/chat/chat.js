af0cd73a3930eda791b5949e736011144da41285
Constantine Simm <constantine.peschanov@gmail.com>
2020-03-29 18:45:06 +0300

    finished third task

--- modules/chat/chat.js
+++ modules/chat/chat.js
@@ -2,11 +2,30 @@ module.exports = (app) => {
   const server = app.listen(3003);
   const io = require('socket.io')(server);
 
+  let usersList = [];
+  let currentAdmin;
+
   io.on('connection', socket => {
       socket.username = 'Anonymous';
-      console.log('New user connected');
+
       socket.on('username_register', (data) => {
           socket.username = data.username;
+
+          //add user to list
+          if (!usersList.includes(socket.username)) {
+              usersList.push(socket.username);
+          }
+
+          //send user connected event
+          io.sockets.emit('user_connected', { users: usersList });
+
+          //set current admin on connected
+          if (usersList.length) {
+              socket.admin = usersList[0];
+          }
+
+          //send event to current admin
+          io.sockets.emit(socket.admin, { users: usersList });
       });
 
       socket.on('new_message', (data) => {
@@ -14,8 +33,27 @@ module.exports = (app) => {
           io.sockets.emit('new_message', { message: data.message, username: socket.username })
       });
 
-      socket.on('typing', (data) => {
-          socket.broadcast.emit('typing', { username: socket.username });
+      socket.on('typing', () => socket.broadcast.emit('typing', { username: socket.username }));
+
+      socket.on('user_delete', (data) => {
+          usersList.splice(usersList.indexOf(data['user']), 1);
+          socket.broadcast.emit('user_disconnected', { users: usersList });
+          io.sockets.emit(`${ data['user']}_delete`);
+          io.sockets.emit('after_delete', { user: data['user'], reason: data['reason'] });
+      });
+
+      socket.on('disconnected', (user) => {
+          //change admin
+          if (usersList.indexOf(user) === 0) {
+              usersList.splice(usersList.indexOf(user), 1);
+              socket.admin = usersList[0];
+
+              io.sockets.emit(socket.admin, { users: usersList });
+          } else {
+              usersList.splice(usersList.indexOf(user), 1);
+          }
+
+          socket.broadcast.emit('user_disconnected', { users: usersList });
       })
   });
 };
--- public/javascripts/chat-socket.js
+++ public/javascripts/chat-socket.js
@@ -6,39 +6,132 @@
     const chatStorage = localStorage.getItem('chat');
     const usernameRegisterButton = $('#chat__username_save');
     const message = $('#chat__message_input');
-    const send_message = $("#chat__message_send");
-    const chatroom = $("#chat__room");
-    const feedback = $("#chat__feedback");
+    const send_message = $('#chat__message_send');
+    const chatroom = $('#chat__room');
+    const feedback = $('#chat__feedback');
+    const usersList = $('#chat__users_list');
 
     //checking chat storage
     if (!chatStorage) {
-        usernameRegisterButton.click(function(){
-            socket.emit('username_register', { username : $('#chat__username_input').val() })
-        });
+        usernameRegisterButton.click(() => socket.emit('username_register', { username : $('#chat__username_input').val() }));
     } else {
-        socket.emit('username_register', { username : JSON.parse(chatStorage).username })
+        socket.emit('username_register', { username : JSON.parse(chatStorage).username });
     }
 
+    //Listen on users connected
+    socket.on('user_connected', (data) => {
+        usersList.html('');
+        data['users'].forEach(user => {
+            usersList.append(`<p id="chat__username"><i class="far fa-user"></i> ${ user }</p>`)
+        });
 
-    //Emit message
-    send_message.click(function(){
-        socket.emit('new_message', {message : message.val()})
+        $('#chat__username').html(`<span class="badge badge-success">Admin</span> ${ data['users'][0] }`)
     });
 
+    //Emit message
+    send_message.click(() => socket.emit('new_message', { message : message.val() }));
+
     //Listen on new_message
-    socket.on("new_message", (data) => {
+    socket.on('new_message', (data) => {
         feedback.html('');
         message.val('');
-        chatroom.append("<p class='message'>" + "<span class='badge badge-primary mr-1'>" + data.username + "</span>" + data.message + "</p>")
+        chatroom.append(`<p class="message"><span class="badge badge-primary mr-1">${ data.username }</span> ${ data.message }</p>`)
     });
 
     //Emit typing
-    message.bind("keypress", () => {
-        socket.emit('typing');
-    });
+    message.bind('keypress', () => socket.emit('typing'));
 
     //Listen on typing
-    socket.on('typing', (data) => {
-        feedback.html("<p id='chat__feedback_msg'><i>" + data.username + " is typing a message..." + "</i></p>")
-    })
+    socket.on('typing', (data) => feedback.html(`<p id="chat__feedback_msg"><i> ${ data.username } is typing a message...</i></p>`))
+
+    //Listen after user delete
+    socket.on('after_delete', (data) => {
+        feedback.html(`<p id="chat__feedback_msg">Admin deleted user "${ data['user'] }" with reason: <i>${ data['reason'] }</i></p>`);
+
+        setTimeout(() => feedback.html(''), 3000);
+    });
+
+    //Listen on user disconnected
+    socket.on('user_disconnected', (data) => {
+        usersList.html('');
+        data['users'].forEach(user => {
+            usersList.append(`<p id="chat__username">${user}</p>`);
+        });
+
+        $('#chat__username').html(`${ data['users'][0] } <span class="badge badge-success">Admin</span>`)
+    });
+
+    if (chatStorage) {
+        //Listen admin event
+        socket.on(JSON.parse(chatStorage).username, (data) => {
+            usersList.html('');
+            data['users'].forEach(user => {
+                usersList.append(`<p id="chat__username"><i class="far fa-user"></i> ${ user } <span class="chat__delete_user" ><i id="chat__delete_user" class="fas fa-times text-danger" data-user="${ user }"></i></span> </p>`)
+            });
+
+            $('#chat__username').html(`<span class="badge badge-success">Admin</span> ${ data['users'][0] }`)
+        });
+
+        //Listen on user delete
+        socket.on(`${ JSON.parse(chatStorage).username }_delete`, () => {
+            localStorage.removeItem('chat');
+
+            document.location.reload();
+        });
+    }
+
+    document.addEventListener('DOMContentLoaded', () => {
+        const usernameRegisterModal = document.getElementById('chat__username_signup');
+        const usernameRegisterInput = document.getElementById('chat__username_input');
+        const usernameRegisterButton = document.getElementById('chat__username_save');
+
+        //remove all data and reload page
+        document.getElementById('chat__clear_confirm_button').onclick = () => {
+            socket.emit('disconnected', JSON.parse(localStorage.getItem('chat')).username);
+            localStorage.removeItem('chat');
+
+            document.location.reload();
+        };
+
+        //check for username register
+        if (!localStorage.getItem('chat') || !JSON.parse(localStorage.getItem('chat')).register) {
+            //show username register modal
+            $(usernameRegisterModal).modal({ backdrop: 'static'}, 'show');
+
+            //toggle save button disabled attribute
+            usernameRegisterInput.oninput = event => event.target.value.length > 0 ? usernameRegisterButton.removeAttribute('disabled') : usernameRegisterButton.setAttribute('disabled', 'disabled');
+
+            usernameRegisterButton.onclick = () => {
+                //save storage value
+                localStorage.setItem('chat', JSON.stringify({ register: true, username: usernameRegisterInput.value }))
+
+                //hide modal
+                $(usernameRegisterModal).modal('hide');
+            }
+        }
+
+        //check message input and toogle send button disabled attribute
+        document.getElementById('chat__message_input').oninput = event => event.target.value.length > 0 ? document.getElementById('chat__message_send').removeAttribute('disabled') : document.getElementById('chat__message_send').setAttribute('disabled', 'disabled');
+
+        //delete user
+        document.onclick = event => {
+            if (event.target.parentElement.id === 'chat__delete_user') {
+                let username = event.target.parentElement.dataset.user;
+                let userRecord = event.target.parentElement.parentElement.parentElement;
+
+                $('#chat__delete_user_confirm').modal('show');
+
+                document.getElementById('chat__delete_user_reason').oninput = event => event.target.value.length > 0 ? document.getElementById('chat__delete_user_button').removeAttribute('disabled') : document.getElementById('chat__delete_user_button').setAttribute('disabled', 'disabled');
+
+                document.getElementById('chat__delete_user_button').onclick = () => {
+                    socket.emit('user_delete', { user: username, reason:  $('#chat__delete_user_reason').val() });
+                    $('#chat__delete_user_confirm').modal('hide');
+                    $('#chat__delete_user_reason').val('');
+
+                    userRecord.remove();
+                };
+            }
+        };
+
+    });
 })();
